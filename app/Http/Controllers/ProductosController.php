9a9e59aa2e5a6453b20f2d3d63910fa39ebd6e21
leonardopadillla <leonardo.umana@catolica.edu.sv>
2022-04-23 11:33:50 -0600

    Initial commit

---
+++ app/Http/Controllers/ProductosController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Productos;
+use Illuminate\Http\Request;
+
+class ProductosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $dataproduct=Productos::all();
+        return view('index', compact('dataproduct'));
+        return redirect('index');
+        //return view('index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('create');
+        return redirect('index');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $dataProducts = $request->except('_token','saveitem');
+        Productos::insert($dataProducts);
+        return response()->json($dataProducts);
+        return redirect('productos');
+       
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Productos  $productos
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Productos $productos)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Productos  $productos
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Productos $productos)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Productos  $productos
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Productos $productos)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Productos  $productos
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Productos $productos)
+    {
+        $dataprod = Productos::where('id', $productos->id)->first();
+        $dataprod->delete();
+        return redirect('productos/');
+
+    }
+}
---
+++ app/Models/Productos.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+//use Illuminate\Database\Eloquent\Factories\HasFactory;
+//use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+
+class Productos extends Eloquent
+{
+ protected $connection = 'mongodb';
+ protected $collection = 'productos';
+}
